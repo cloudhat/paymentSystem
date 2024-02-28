96d1c6c36ff2ded43b03f1c9053a52975027ec03
cloudhat <cloudhat17@gmail.com>
2024-02-28 15:22:45 +0900

    init

---
+++ src/main/java/com/paymentsystemex/PaymentSystemExApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsystemex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemExApplication.class, args);
+	}
+
+}
