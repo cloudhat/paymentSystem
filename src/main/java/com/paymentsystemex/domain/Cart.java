bbd8f3ed1ba069d7dd371937da3211c1215d580d
cloudhat <cloudhat17@gmail.com>
2024-03-20 15:23:49 +0900

    장바구니 구현

--- src/main/java/com/paymentsystemex/domain/Cart.java
+++ src/main/java/com/paymentsystemex/domain/Cart.java
@@ -2,7 +2,6 @@ package com.paymentsystemex.domain;
 
 import com.paymentsystemex.domain.member.Member;
 import com.paymentsystemex.domain.product.Product;
-import com.paymentsystemex.domain.product.ProductOption;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,29 +18,18 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "productOption_id", nullable = false)
     private ProductOption productOption;
 
     @Column
     private int quantity;
-
-    public Cart(Member member, Product product ,ProductOption productOption, int quantity) {
-        this.member = member;
-        this.product = product;
-        this.productOption = productOption;
-        this.quantity = quantity;
-    }
-
-    public void updateQuantity(int delta){
-        this.quantity = quantity + delta;
-    }
 }
