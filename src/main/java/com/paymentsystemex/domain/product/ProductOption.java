b5123a80f3f845b6a1162aea2563b63e2fc63539
kimsan <cloudhat17@gmail.com>
2024-03-25 23:15:06 +0900

    임시

--- src/main/java/com/paymentsystemex/domain/product/ProductOption.java
+++ src/main/java/com/paymentsystemex/domain/product/ProductOption.java
@@ -3,6 +3,7 @@ package com.paymentsystemex.domain.product;
 import com.paymentsystemex.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class ProductOption {
 
@@ -36,5 +38,13 @@ public class ProductOption {
     public boolean isCurrentlyAvailable() {
         LocalDateTime now = LocalDateTime.now();
         return (saleStartDt == null || saleStartDt.isBefore(now)) && (saleEndDt == null || saleEndDt.isAfter(now));
+    }
+
+    public void setProduct(Product product){
+        if(this.product != null){
+            this.product.getProductOptions().remove(this);
+        }
+        this.product = product;
+        product.getProductOptions().add(this);
     }
 }
