4cfac6ebd93b032037181279a3edc4f714358b7e
cloudhat <67942847+cloudhat@users.noreply.github.com>
2024-04-08 17:54:28 +0900

    Revert "임시"
    This reverts commit 3efc162472a19affa63df259a31c523e04dcd44b.

--- src/main/java/com/paymentsystemex/domain/product/ProductOption.java
+++ src/main/java/com/paymentsystemex/domain/product/ProductOption.java
@@ -3,7 +3,6 @@ package com.paymentsystemex.domain.product;
 import com.paymentsystemex.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +10,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 public class ProductOption {
 
@@ -38,13 +36,5 @@ public class ProductOption {
     public boolean isCurrentlyAvailable() {
         LocalDateTime now = LocalDateTime.now();
         return (saleStartDt == null || saleStartDt.isBefore(now)) && (saleEndDt == null || saleEndDt.isAfter(now));
-    }
-
-    public void setProduct(Product product){
-        if(this.product != null){
-            this.product.getProductOptions().remove(this);
-        }
-        this.product = product;
-        product.getProductOptions().add(this);
     }
 }
