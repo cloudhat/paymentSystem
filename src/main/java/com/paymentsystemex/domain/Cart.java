a38ec3b4d9737b7ff74b865021c56202064d8897
cloudhat <67942847+cloudhat@users.noreply.github.com>
2024-04-08 17:54:28 +0900

    Revert "엔티티 추가"
    This reverts commit bf1b6636d33b3dacce389d48086efef9a7fa23ea.

--- src/main/java/com/paymentsystemex/domain/Cart.java
+++ src/main/java/com/paymentsystemex/domain/Cart.java
@@ -2,6 +2,7 @@ package com.paymentsystemex.domain;
 
 import com.paymentsystemex.domain.member.Member;
 import com.paymentsystemex.domain.product.Product;
+import com.paymentsystemex.domain.product.ProductOption;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,18 +19,34 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "productOption_id", nullable = false)
     private ProductOption productOption;
 
     @Column
     private int quantity;
+
+    public Cart(Member member, Product product ,ProductOption productOption, int quantity) {
+        this.member = member;
+        this.product = product;
+        this.productOption = productOption;
+        this.quantity = quantity;
+    }
+
+    public void updateQuantity(int changeAmount){
+
+        if(getQuantity() + changeAmount <=0){
+            throw new IllegalArgumentException();
+        }
+
+        this.quantity = quantity + changeAmount;
+    }
 }
