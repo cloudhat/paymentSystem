d0b6e10ea9cead5d041ed6314ff3aab8b2a1a720
cloudhat <67942847+cloudhat@users.noreply.github.com>
2024-04-08 17:54:28 +0900

    Revert "temp"
    This reverts commit 7ce619078a70c9452f5ac88535826f94ea95fbef.

--- src/main/java/com/paymentsystemex/repository/CartRepository.java
+++ src/main/java/com/paymentsystemex/repository/CartRepository.java
@@ -1,39 +1,12 @@
 package com.paymentsystemex.repository;
 
 import com.paymentsystemex.domain.Cart;
-import com.paymentsystemex.domain.member.Member;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import static com.paymentsystemex.domain.QCart.cart;
-import static com.paymentsystemex.domain.product.QProductOption.productOption;
-
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
-@RequiredArgsConstructor
-@Repository
-public class CartRepository {
-    private final JPAQueryFactory queryFactory;
-    private EntityManager em;
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByMemberId(Long memberId);
 
-    public Cart save(Cart cart){
-        em.persist(cart);
-
-        return cart;
-    }
-
-    public List<Cart> findByMemberId(Long memberId){
-
-        LocalDateTime now = LocalDateTime.now();
-
-        return queryFactory
-                .selectFrom(cart)
-                .join(cart.productOption,productOption) //TODO : 이 줄 삭제하고 테스트해보기
-                .where(cart.member.id.eq(memberId)
-                        .and(productOption.saleStartDt.loe(now).and(productOption.saleEndDt.goe(now))))
-                .fetch();
-    }
 }
